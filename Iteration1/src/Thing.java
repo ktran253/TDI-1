a3e6e51dac1fcbbb863a77b3e6190558a37046ea
Zac Moriarty <cevis906@uw.edu>
2022-05-27 16:28:54 -0700

    Added JSON dependency and Things List

--- Iteration1/src/Thing.java
+++ Iteration1/src/Thing.java
@@ -1,44 +1,66 @@
 import java.util.Comparator;
 
-public class Thing{
+public class Thing {
     private String name, room, type;
     private String description;
-    public Thing(String name, String room, String type, String description){
+
+    public Thing(String name, String room, String type, String description) {
         this.name = name;
         this.room = room;
         this.type = type;
         this.description = description;
     }
-    public String getName(){return name;}
 
-    public String getRoom() {return room;}
+    public String getName() {
+        return name;
+    }
 
-    public String getType() {return type;}
+    public String getRoom() {
+        return room;
+    }
 
-    public String getDescription() {return description;}
+    public String getType() {
+        return type;
+    }
 
-    public void setName(String name) {this.name = name;}
+    public String getDescription() {
+        return description;
+    }
 
-    public void setRoom(String room) {this.room = room;}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public void setType(String type) {this.type = type;}
+    public void setRoom(String room) {
+        this.room = room;
+    }
 
-    public void setDescription(String description) {this.description = description;}
+    public void setType(String type) {
+        this.type = type;
+    }
 
-    public String[] getObject(){return new String[] {name, room, type}; }
-}
-class SortByName implements Comparator<Thing> {
-    public int compare(Thing one, Thing two){
-        return one.getName().compareTo(two.getName());
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String[] getObject() {
+        return new String[]{name, room, type};
     }
 }
-class SortByRoom implements Comparator<Thing> {
-    public int compare(Thing one, Thing two){
-        return one.getRoom().compareTo(two.getRoom());
+    class SortByName implements Comparator<Thing> {
+        public int compare(Thing one, Thing two) {
+            return one.getName().compareTo(two.getName());
+        }
     }
-}
-class SortByType implements Comparator<Thing> {
-    public int compare(Thing one, Thing two){
-        return one.getType().compareTo(two.getType());
+
+    class SortByRoom implements Comparator<Thing> {
+        public int compare(Thing one, Thing two) {
+            return one.getRoom().compareTo(two.getRoom());
+        }
     }
-}
+
+    class SortByType implements Comparator<Thing> {
+        public int compare(Thing one, Thing two) {
+            return one.getType().compareTo(two.getType());
+        }
+    }
