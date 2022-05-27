58669a8b35b59d2f450952572c52a0bf749371e0
ktran253 <103085011+ktran253@users.noreply.github.com>
2022-05-27 15:54:42 -0700

    Add files via upload

---
+++ Iteration1/src/addType.java
@@ -0,0 +1,61 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+/**
+ * Add Type
+ */
+public class addType extends JDialog {
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+    private JLabel typeLabel;
+    private JTextField textField1;
+
+    public addType() {
+        setVisible(true);
+        setSize(200, 150);
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
+        buttonCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() when cross is clicked
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() on ESCAPE
+        contentPane.registerKeyboardAction(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+    }
+
+    public JButton getButtonOK() {
+        return buttonOK;
+    }
+
+    public String getText() {
+        return textField1.getText();
+    }
+
+    private void onCancel() {
+        // add your code here if necessary
+        dispose();
+    }
+
+    public static void main(String[] args) {
+        addType dialog = new addType();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }
+}
